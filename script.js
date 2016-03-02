67bd054cd5c6c233093245b89131d121415e4378
xhesika <xhesmx98@gmail.com>
2016-03-02 21:13:15 +0000

    removed assigment instructions

--- script.js
+++ script.js
@@ -5,12 +5,7 @@ secondColor = "";
 
 $(document).ready(function() {
   
-  // STEP ONE //
-  // attach a click event listener to all elements with the class picture-box
-  // we need to execute the function turnSquare every time a square is clicked
-  // but we need to pass a parameter $(this) to the turnSquare function
-  // to do this, we need to use the .click(function() { turnSquare( $(this) ) }) format
-  
+ 
   $(".picture-box").click(function() { turnSquare( $(this) ) } )
 });
 
@@ -18,22 +13,16 @@ $(document).ready(function() {
 function turnSquare(squareObject)
 {
   
-  // STEP TWO
-  // increment (add) 1 to the variable clickCount
-  // it will look something like this clickCount = 
+ 
 clickCount= clickCount + 1;
 
    
   $("#clickCount").html(clickCount);
   
-  // this line gets the data attribute that holds the hidden square color
+  
   secretSquareColor = squareObject.data("color");
   squareObject.css("background-color", secretSquareColor);
    
-  // STEP THREE
-  // create a conditional that says 
-  // if the click count equals 1, set the variable firstColor to the value of secretSquareColor
-  // if the click count equals 2, set the variable secondColor to the value of secretSquareColor and then execute the function checkMatches
   
   if (clickCount== 1) { firstColor = secretSquareColor;}
    if (clickCount== 2) { firstColor = secretSquareColor;
@@ -45,16 +34,13 @@ clickCount= clickCount + 1;
 
 function checkMatches(){
   
-  // STEP FOUR
-  //create a conditional that says if firstColor is the same as secondColor, alert to the user "You Win!"
-  // else alert to the user "Try again!"
-   if ( firstColor==secondColor ) {alert("YOU WOM") }
-     else { alert("congrats you played youself and this game") }
+ 
+   if ( firstColor == secondColor ) {alert("YOU WON") }
+     else { alert("YOU LOSE") }
   
   
 
-  // STEP FIVE
-  //execute the function resetSquares
+
   
   resetSquares();
 
@@ -63,16 +49,14 @@ function checkMatches(){
 
 function resetSquares()
 {
-  // STEP SIX
-  // reset the clickCount variablel to 0
   
   
+  clickCount=0
   
   $("#clickCount").html(clickCount);
   
   
-  // STEP SEVEN
-  // change all elements with the class picture-box to have a background color of #cccccc
   
+   $(".picture-box").css("background-color","#cccccc");
 
 }
